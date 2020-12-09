42d7c38033fd1fe5d37a65961d6172ec73e51bdb
tofeqabu <57616702+tofeqabu@users.noreply.github.com>
2020-12-09 14:41:01 +0200

    Update config.php

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "nemer";
+$dbname = "plan2plant";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
